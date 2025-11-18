ad682e42b43a7f9b43a853845bb59ce044e7a183
Adiraj <adirajk03@gmail.com>
2025-11-18 19:33:45 +0530

    Update Profiles.tsx

--- src/pages/Profiles.tsx
+++ src/pages/Profiles.tsx
@@ -8,7 +8,7 @@ const profiles = [
     subtitle: "Developer",
     instagram: "@adiraj",
     instagramUrl: "https://instagram.com/adiraj_ark",
-    image: "/Images/adi.png"
+    image: "/Images/adi(1).png"
   },
   {
     name: "Angsuman",
